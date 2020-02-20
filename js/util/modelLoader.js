8971840b0a25ece6abcbd10377ffa069117c26f1
Andy <russellandy98@gmail.com>
2020-02-20 15:00:18 -0500

    demo week 4

--- js/util/modelLoader.js
+++ js/util/modelLoader.js
@@ -1,6 +1,8 @@
-import { scene } from '../index2.js'
+import { scene, dynamicObjects, loadingManager, mixers } from '../index2.js'
+import { physicsWorld } from './physics.js'
+
 export function modelLoader( path, pos, name ){
-	const loader = new THREE.GLTFLoader();
+	const loader = new THREE.GLTFLoader(loadingManager);
 	loader.load(path, 
 	(model) => onLoad(model, pos, name),
 	() => progress(),
@@ -8,11 +10,37 @@ export function modelLoader( path, pos, name ){
 }
 
 function onLoad( model, pos, name ){
-    const pokemon = model.scene
-    pokemon.position.copy(pos)
-    pokemon.name = name
-    scene.add(pokemon)
-    // console.log(pika)
+    let bbox = null
+    const character = model.scene
+    character.scale.set(0.005, 0.005, 0.005)
+    
+    var box = new THREE.Box3().setFromObject( character );
+    let test = box.getSize(new THREE.Vector3())
+
+    bbox = new Ammo.btBoxShape( new Ammo.btVector3( test.x, test.y, test.z ) );
+    bbox.setMargin( 0.05 );
+
+    character.position.copy(pos)
+    character.name = name
+    var mass = 3 * 5;
+    var localInertia = new Ammo.btVector3( 0, 0, 0 );
+    bbox.calculateLocalInertia( mass, localInertia );
+    var transform = new Ammo.btTransform();
+    transform.setIdentity();
+    transform.setOrigin( new Ammo.btVector3( pos.x, pos.y, pos.z ) );
+    var motionState = new Ammo.btDefaultMotionState( transform );
+    var rbInfo = new Ammo.btRigidBodyConstructionInfo( mass, motionState, bbox, localInertia );
+    var body = new Ammo.btRigidBody( rbInfo );
+    character.userData.physicsBody = body
+    dynamicObjects.push( character )
+    scene.add(character)
+
+    let mixer = new THREE.AnimationMixer( character );
+    mixers.push(mixer);
+    mixer.clipAction(model.animations[0]).play();
+
+    //physicsWorld.addRigidBody( body );
+    console.log(physicsWorld, scene)
 
 }
 
--- js/util/terrain.js
+++ js/util/terrain.js
@@ -1,23 +1,42 @@
+
 export function Terrain() {
 
     let xS = 63, yS = 63;
 
-    let mntTexture = new THREE.TextureLoader().load('../assets/textures/grass.jpg');
-    mntTexture.wrapS = mntTexture.wrapT = THREE.RepeatWrapping;
-    mntTexture.repeat.set(16,16);
+    //let mntTexture = new THREE.TextureLoader().load('../assets/textures/grass.jpg');
+    //mntTexture.wrapS = mntTexture.wrapT = THREE.RepeatWrapping;
+    //mntTexture.repeat.set(8,8);
+	//let t1 = new THREE.TextureLoader().load('../assets/textures/testWater.jpg');
+	let t1 = new THREE.TextureLoader().load('../assets/textures/testSand.jpg');
+	let t2 = new THREE.TextureLoader().load('../assets/textures/testGrass.jpg');
+	let t3 = new THREE.TextureLoader().load('../assets/textures/testRock.jpg');
+	
+	var material = THREE.Terrain.generateBlendedMaterial([
+    // The first texture is the base; other textures are blended in on top.
+    {texture: t1, levels: [-10, -5, ,15 , 25]},
+    // Start blending in at height -80; opaque between -35 and 20; blend out by 50
+    {texture: t2, levels: [5, 10, 45, 60]},
+    {texture: t3, levels: [30, 50, 100, 100]},
+
+    // How quickly this texture is blended in depends on its x-position.
+    //{texture: t4, glsl: '1.0 - smoothstep(65.0 + smoothstep(-256.0, 256.0, vPosition.x) * 10.0, 80.0, vPosition.z)'},
+    // Use this texture if the slope is between 27 and 45 degrees
+    //{texture: t3, glsl: 'slope > 0.7853981633974483 ? 0.2 : 1.0 - smoothstep(0.47123889803846897, 0.7853981633974483, slope) + 0.2'},
+	]);
+	
     let terrainScene = THREE.Terrain({
         easing: THREE.Terrain.Linear,
         frequency: 3.5,
         heightmap: THREE.Terrain.HillIsland,
-        material: new THREE.MeshBasicMaterial({map:mntTexture,side: THREE.DoubleSide}),
-        maxHeight: 1000,
-        minHeight: -100,
+        material: material,
+        maxHeight: 100,
+        minHeight: -10,
         steps: 1,
         useBufferGeometry: false,
         xSegments: xS,
-        xSize: 4096,
+        xSize: 2048,
         ySegments: yS,
-        ySize: 4096,
+        ySize: 2048,
     });
     // Assuming you already have your global scene, add the terrain to it
 
@@ -33,6 +52,25 @@ export function Terrain() {
         randomness: Math.random,
     });
     terrainScene.add(decoScene);
-
+    var test = THREE.Terrain.toHeightmap(
+        // terrainScene.children[0] is the most detailed version of the terrain mesh
+        terrainScene.children[0].geometry.vertices,
+        { xSegments: 63, ySegments: 63 }
+    );
+    let data = terrainScene.children[0].geometry
+    console.log(THREE.Terrain.heightmapArray(THREE.Terrain.HillIsland,{
+        easing: THREE.Terrain.Linear,
+        frequency: 3.5,
+        material: new THREE.MeshBasicMaterial({map:material,side: THREE.DoubleSide}),
+        maxHeight: 100,
+        minHeight: -10,
+        steps: 1,
+        useBufferGeometry: false,
+        xSegments: xS,
+        xSize: 2048,
+        ySegments: yS,
+        ySize: 2048
+    }))
+    console.log(terrainScene.children[0].geometry.vertices)
     return terrainScene
 }
